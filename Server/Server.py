fe75a0ae22bfe5ec60de905b4767c4bd3ef2dcb8
Hero45678 <pi@ISAACSPI.home>
2021-02-10 17:42:05 +0000

    Initial commit from raspi, server started

---
+++ Server/Server.py
@@ -0,0 +1,19 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route("/")
+def home():
+    # return "Welcome to the home page"
+    return flask.render_template("home.html")
+
+@app.route("/error_testing")
+def error_testing():
+    return "", 304
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0", 
+        port=80,
+        debug=True
+    )
